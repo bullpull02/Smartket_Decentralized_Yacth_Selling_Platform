eeddab583ba5acf42c9cc8ccbac70bc269d4000f
bullpull02 <mykolazbudchak@gmail.com>
2023-05-25 21:44:46 -0400

    update header

--- smartket-frontend/src/layouts/MainLayout/Header.tsx
+++ smartket-frontend/src/layouts/MainLayout/Header.tsx
@@ -61,76 +61,74 @@ const Header = () => {
 
   return (
     <AppBar position='sticky'>
-      <Toolbar>
-        <div className='container'>
-          <div className='flex h-16 items-center justify-between'>
-            <Link to='/'>
-              <img
-                src='/images/logo.png'
-                alt=''
-                className='h-14 -translate-x-4 drop-shadow-[2px_2px_4px_#fff4]'
-              />
-            </Link>
-            <div className='flex items-center space-x-8'>
-              <nav>
-                <ul className='flex items-center space-x-8'>
-                  {menu.map(
-                    (menuItem, ind) =>
-                      (menuItem.auth === isLoggedIn || menuItem.auth === undefined) && (
-                        <li
-                          className='relative flex h-16 items-center'
-                          onMouseLeave={() => setHoveredMenu('')}
-                          key={ind}
+      <div className='container'>
+        <div className='flex h-16 items-center justify-between'>
+          <Link to='/'>
+            <img
+              src='/images/logo.png'
+              alt=''
+              className='h-14 -translate-x-4 drop-shadow-[2px_2px_4px_#fff4]'
+            />
+          </Link>
+          <div className='flex items-center space-x-8'>
+            <nav>
+              <ul className='flex items-center space-x-8'>
+                {menu.map(
+                  (menuItem, ind) =>
+                    (menuItem.auth === isLoggedIn || menuItem.auth === undefined) && (
+                      <li
+                        className='relative flex h-16 items-center'
+                        onMouseLeave={() => setHoveredMenu('')}
+                        key={ind}
+                      >
+                        <span
+                          className='cursor-pointer hover:opacity-80'
+                          onMouseOver={() => setHoveredMenu(menuItem.label)}
+                          onClick={() => handleClickLink(menuItem.to)}
                         >
-                          <span
-                            className='cursor-pointer hover:opacity-80'
-                            onMouseOver={() => setHoveredMenu(menuItem.label)}
-                            onClick={() => handleClickLink(menuItem.to)}
-                          >
-                            {menuItem.label}
-                          </span>
-                          <ul
-                            className={cx(
-                              'trans absolute bottom-0 left-0 w-fit translate-y-full divide-y divide-gray-500 divide-opacity-20 border border-gray-800 bg-gray-800 shadow',
-                              menuItem.submenu && hoveredMenu === menuItem.label
-                                ? 'pointer-events-auto opacity-100'
-                                : 'pointer-events-none opacity-0',
-                            )}
-                          >
-                            {menuItem.submenu?.map((submenu) => (
-                              <li className='group relative' key={submenu.label}>
-                                <div
-                                  className='cursor-pointer whitespace-nowrap px-3 py-2 text-sm hover:bg-gray-900 hover:bg-opacity-80'
-                                  onClick={() => handleClickLink(submenu.to)}
-                                >
-                                  {submenu.label}
-                                </div>
-                                <ul className='trans pointer-events-none absolute right-0 top-0 translate-x-full divide-y divide-gray-500 divide-opacity-20 bg-gray-800 opacity-0 group-hover:pointer-events-auto group-hover:opacity-100'>
-                                  {submenu.submenu?.map((subSubmenu) => (
-                                    <li key={subSubmenu.label}>
-                                      <div
-                                        className='cursor-pointer whitespace-nowrap px-3 py-2 text-sm shadow hover:bg-gray-900 hover:bg-opacity-80'
-                                        onClick={() => handleClickLink(subSubmenu.to)}
-                                      >
-                                        {subSubmenu.label}
-                                      </div>
-                                    </li>
-                                  ))}
-                                </ul>
-                              </li>
-                            ))}
-                          </ul>
-                        </li>
-                      ),
-                  )}
-                </ul>
-              </nav>
-              <WalletConnectButton />
-              {isLoggedIn && <ProfileMenu />}
-            </div>
+                          {menuItem.label}
+                        </span>
+                        <ul
+                          className={cx(
+                            'trans absolute bottom-0 left-0 w-fit translate-y-full divide-y divide-gray-500 divide-opacity-20 border border-gray-800 bg-gray-800 shadow',
+                            menuItem.submenu && hoveredMenu === menuItem.label
+                              ? 'pointer-events-auto opacity-100'
+                              : 'pointer-events-none opacity-0',
+                          )}
+                        >
+                          {menuItem.submenu?.map((submenu) => (
+                            <li className='group relative' key={submenu.label}>
+                              <div
+                                className='cursor-pointer whitespace-nowrap px-3 py-2 text-sm hover:bg-gray-900 hover:bg-opacity-80'
+                                onClick={() => handleClickLink(submenu.to)}
+                              >
+                                {submenu.label}
+                              </div>
+                              <ul className='trans pointer-events-none absolute right-0 top-0 translate-x-full divide-y divide-gray-500 divide-opacity-20 bg-gray-800 opacity-0 group-hover:pointer-events-auto group-hover:opacity-100'>
+                                {submenu.submenu?.map((subSubmenu) => (
+                                  <li key={subSubmenu.label}>
+                                    <div
+                                      className='cursor-pointer whitespace-nowrap px-3 py-2 text-sm shadow hover:bg-gray-900 hover:bg-opacity-80'
+                                      onClick={() => handleClickLink(subSubmenu.to)}
+                                    >
+                                      {subSubmenu.label}
+                                    </div>
+                                  </li>
+                                ))}
+                              </ul>
+                            </li>
+                          ))}
+                        </ul>
+                      </li>
+                    ),
+                )}
+              </ul>
+            </nav>
+            <WalletConnectButton />
+            {isLoggedIn && <ProfileMenu />}
           </div>
         </div>
-      </Toolbar>
+      </div>
     </AppBar>
   )
 }
